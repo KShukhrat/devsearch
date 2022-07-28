1be5aeba472023959a866e4e19c1dd27d823736a
QShuhrat <93004364+QShuhrat@users.noreply.github.com>
2022-07-29 03:58:39 +0500

    adsfa

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.projects, name='projects'),
-    path('project/<str:pk>/', views.project, name='project'),
+    path('', projects, name='projects'),
+    path('project/<str:pk>/', project, name='project'),
 
-    path('create-project/', views.CreateProject, name='create-project'),
+    path('create-project/', CreateProject, name='create-project'),
+    path('update-project/<str:pk>/', UpdateProject, name='update-project'),
+    path('delete-project/<str:pk>/', DeleteProject, name='delete-project'),
 
 ]
--- projects/views.py
+++ projects/views.py
@@ -22,3 +22,22 @@ def CreateProject(request):
             return redirect('projects')
     context = {'form': form}
     return render(request,'projects/project_form.html',context)
+
+def UpdateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form =ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form': form}
+    return  render(request, 'projects/project_form.html', context)
+
+def DeleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object': project}
+    return render(request,'projects/delete-template.html', context)
