a8f2162000a72824006b49c144734d84cdb00f28
QShuhrat <93004364+QShuhrat@users.noreply.github.com>
2022-08-01 02:53:01 +0500

    fasdfad

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
 from .models import *
 from django import forms
 
@@ -12,11 +12,10 @@ class ProjectForm(ModelForm):
         }
 
 
-    def __int__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(ProjectForm, self).__init__(*args, **kwargs)
         for name, field in self.fields.items():
             field.widget.attrs.update({'class': 'input'})
+
         # self.fields['title'].widget.attrs.update({'class': 'input'})
-
-
         # self.fields['description'].widget.attrs.update({'class': 'input'})
